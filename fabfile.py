fae6fbe47ad290189d50f44d197297038f7c8d7a
danielfm <daniel.tritone@gmail.com>
2009-11-15 23:44:03 -0200

    Now the build script depends on Fabric v0.9+. Started using the Google Closure Compiler API to minify the source code. Test Suite now links to the minified version of JsHamcrest.

--- fabfile.py
+++ fabfile.py
@@ -1,118 +1,142 @@
 #-*- coding:utf-8 -*-
 
-"""
+"""JsHamcrest build script.
 """
 
 import re
-from StringIO import StringIO
+import cStringIO as StringIO
+
+from fabric.api import *
+
 
 # Project
-config.project   = 'jshamcrest'
-config.version   = '0.5.2'
-config.full_name = '%s-%s' % (config.project, config.version)
+env.project   = 'jshamcrest'
+env.version   = '0.5.2'
+env.full_name = '%s-%s' % (env.project, env.version)
 
 # Build output
-config.build_dir     = 'build'
-config.dist_dir      = 'dist'
-config.rev_info_file = '%s/_rev_info.txt' % (config.build_dir,)
+env.build_dir     = 'build'
+env.dist_dir      = 'dist'
+env.rev_info_file = '%s/_rev_info.txt' % (env.build_dir,)
+
+# Output script files
+env.js             = '%s/%s.js' % (env.build_dir, env.project)
+env.js_version     = '%s/%s.js' % (env.build_dir, env.full_name)
+env.js_min         = '%s/%s-min.js' % (env.build_dir, env.project)
+env.js_min_version = '%s/%s-min.js' % (env.build_dir, env.full_name)
+
+# Test
+env.test_dir    = 'test'
+env.web_browser = 'firefox'
 
 # Documentation
-config.doc_dir = 'doc'
+env.doc_dir = 'doc'
 
 # Source code
-config.src_dir   = 'src'
-config.src_files = ('jshamcrest',
-                    'core',
-                    'number',
-                    'text',
-                    'object',
-                    'collection',
-                    'integration',)
+env.src_dir   = 'src'
+env.src_files = ('jshamcrest',
+                 'core',
+                 'number',
+                 'text',
+                 'object',
+                 'collection',
+                 'integration',)
 
 # Remote server
-config.fab_hosts = ('destaquenet.com',)
+env.fab_hosts = ('destaquenet.com',)
 
 # Constants
-PATTERN_COMMIT_HASH = re.compile('commit\W+([0-9a-f]+)')
-PATTERN_COMMIT_DATE = re.compile('Date:\W+(.*)')
+_PATTERN_COMMIT_HASH = re.compile('commit\W+([0-9a-f]+)')
+_PATTERN_COMMIT_DATE = re.compile('Date:\W+(.*)')
 
+
+@runs_once
 def clean():
     """Resets the build output directories.
     """
-    local('rm -fR $(build_dir) $(dist_dir)')
-    local('mkdir -p $(build_dir) $(dist_dir)')
+    local('rm -fR %s %s' % (env.build_dir, env.dist_dir))
+    local('mkdir -p %s %s' % (env.build_dir, env.dist_dir))
 
-@depends(clean)
-def set_revision_info():
-    """Reads information about the latest revision.
-    """
-    local('git rev-list --all --max-count=1 --pretty > $(rev_info_file)')
-    rev_info = file(config.rev_info_file, 'r').read()
-    config.commit_hash = PATTERN_COMMIT_HASH.findall(rev_info)[0]
-    config.commit_date = PATTERN_COMMIT_DATE.findall(rev_info)[0]
-
-def read_files():
-    """Reads and joins the source files.
-    """
-    config.src_content = StringIO()
-    for file_name in config.src_files:
-        file_path = '%s/%s.js' % (config.src_dir, file_name)
-        config.src_content.writelines(file(file_path, 'r').readlines())
-
-@depends(read_files)
-def replace_tokens():
-    """Replaces the tokens found in the source code.
-    """
-    content = config.src_content.getvalue()
-    config.src_content = StringIO()
-
-    content = content.replace('@VERSION', config.version)
-    content = content.replace('@REV', config.commit_hash)
-    content = content.replace('@DATE', config.commit_date)
-
-    config.src_content.write(content)
-    config.src_content.seek(0)
-
-@depends(set_revision_info, replace_tokens)
+@runs_once
 def build():
     """Builds the final script and writes it to the disk.
     """
-    content = config.src_content.readlines()
-    for name in [config.project, config.full_name]:
-        output = file('%s/%s.js' % (config.build_dir, name), 'w')
-        output.writelines(content)
+    _set_revision_info()
+    _replace_tokens()
+    content = env.src_content.readlines()
+    file(env.js, 'w').writelines(content)
+    local('cp %s %s' % (env.js, env.js_version))
+        
 
-@depends(build)
 def test():
     """Opens the test suite on a web browser.
     """
-    prompt('web_browser', 'Please choose your web browser', \
-            default='/usr/bin/firefox')
-    local('$(web_browser) test/testSuite.html &')
+    pack()
+    web_browser = prompt('Please choose your web browser', \
+            default=env.web_browser)
+    local('%s %s/testSuite.html &' % (web_browser, env.test_dir))
 
-@depends(build)
+@runs_once
+def doc_clean():
+    """Resets the doc output directories.
+    """
+    local('cd %s; make clean;' % env.doc_dir)
+
 def doc_html():
     """Builds the HTML documentation.
     """
-    local('cd $(doc_dir); make clean; make html')
+    doc_clean()
+    local('cd %s; make html' % env.doc_dir)
 
-@depends(build)
 def doc_pdf():
     """Builds the PDF documentation."
     """
-    local('cd $(doc_dir); make clean; make latex')
-    local('cd $(doc_dir)/_build/latex; make all-pdf')
+    doc_clean()
+    local('cd %s; make latex' % env.doc_dir)
+    local('cd %s/_build/latex; make all-pdf' % env.doc_dir)
 
-@depends(doc_html, doc_pdf)
 def doc():
     """Builds the documentation both in HTML and PDF.
     """
-    pass
+    doc_clean()
+    doc_html()
+    doc_pdf()
 
-@depends(build)
 def pack():
-    """Creates a minified version of the final script.
+    """Creates a minified version of the final script using the Google Closure
+    Compiler service.
     """
-    src = '%s/%s.js' % (config.build_dir, config.full_name)
-    dst = '%s/%s-min.js' % (config.build_dir, config.full_name)
-    local('cat %s | python lib/jsmin.py > %s' % (src, dst))
+    build()
+    local('python lib/closure_compiler_cli.py -f %s > %s' % (env.js, env.js_min))
+    local('cp %s %s' % (env.js_min, env.js_min_version))
+
+def _set_revision_info():
+    """Reads information about the latest revision.
+    """
+    clean()
+    local('git rev-list --all --max-count=1 --pretty > %s' % env.rev_info_file)
+    rev_info = file(env.rev_info_file, 'r').read()
+    env.commit_hash = _PATTERN_COMMIT_HASH.findall(rev_info)[0]
+    env.commit_date = _PATTERN_COMMIT_DATE.findall(rev_info)[0]
+
+def _read_files():
+    """Reads and joins the source files.
+    """
+    env.src_content = StringIO.StringIO()
+    for file_name in env.src_files:
+        file_path = '%s/%s.js' % (env.src_dir, file_name)
+        env.src_content.writelines(file(file_path, 'r').readlines())
+
+def _replace_tokens():
+    """Replaces the tokens found in the source code.
+    """
+    _read_files()
+    content = env.src_content.getvalue()
+    env.src_content = StringIO.StringIO()
+
+    content = content.replace('@VERSION', env.version)
+    content = content.replace('@REV', env.commit_hash)
+    content = content.replace('@DATE', env.commit_date)
+
+    env.src_content.write(content)
+    env.src_content.seek(0)
