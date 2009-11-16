b0035c6e3b10b4a1e53565478c206fc206b1c3a5
danielfm <daniel.tritone@gmail.com>
2009-11-15 23:41:26 -0200

    Updated JsTestDriver (v1.0b -> v1.2).

--- test/integration/jstestdriver/startserver.sh
+++ test/integration/jstestdriver/startserver.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-rm -R build
-mkdir build
-cp ../../../build/jshamcrest.js build
-
 BROWSER_PATH="$1"
 
 if [ -z "$1" ]; then 
 	BROWSER_PATH="`which firefox`"
 fi
 
-java -jar JsTestDriver-1.0b.jar --port 4224 --browser "$BROWSER_PATH"
+java -jar JsTestDriver-1.2.jar --port 4224 --browser "$BROWSER_PATH"
