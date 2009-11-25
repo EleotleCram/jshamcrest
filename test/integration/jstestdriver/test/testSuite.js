b2b261ade84cba139440dcf9bb44d75fc3ce3c3e
danielfm <daniel.tritone@gmail.com>
2009-11-25 18:25:48 -0200

    Changed tab length (4->2).

--- test/integration/jstestdriver/test/testSuite.js
+++ test/integration/jstestdriver/test/testSuite.js
@@ -1,9 +1,9 @@
 IntegrationTest = TestCase("IntegrationTest");
 
 IntegrationTest.prototype.testAssertion = function() {
-    assertThat(5, between(0).and(10), "This assertion must succeed");
+  assertThat(5, between(0).and(10), "This assertion must succeed");
 };
 
 IntegrationTest.prototype.testFailedAssertion = function() {
-    assertThat([], not(empty()), "This assertion must fail");
+  assertThat([], not(empty()), "This assertion must fail");
 };
