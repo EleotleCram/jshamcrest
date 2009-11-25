9626ccd7f5bdd669a42af8ac26712a8c7f18e03c
danielfm <daniel.tritone@gmail.com>
2009-11-25 18:16:50 -0200

    Changed tab length (4->2).

--- test/cases/object.js
+++ test/cases/object.js
@@ -8,58 +8,58 @@ new Test.Unit.Runner({
     }},
 
     testHasMember: function() { with(this) {
-        var hasMember = JsHamcrest.Matchers.hasMember;
-        assert(hasMember('length').matches([]));
-        assert(hasMember('matches').matches(hasMember()));
-        assert(!hasMember('somethingElse').matches([]));
-        assert(!hasMember('somethingElse').matches(null));
+      var hasMember = JsHamcrest.Matchers.hasMember;
+      assert(hasMember('length').matches([]));
+      assert(hasMember('matches').matches(hasMember()));
+      assert(!hasMember('somethingElse').matches([]));
+      assert(!hasMember('somethingElse').matches(null));
     }},
 
     testHasFunction: function() { with(this) {
-        var hasFunction = JsHamcrest.Matchers.hasFunction;
-        assert(hasFunction('matches').matches(hasMember()));
-        assert(!hasFunction('length').matches([]));
-        assert(!hasFunction('somethingElse').matches(null));
+      var hasFunction = JsHamcrest.Matchers.hasFunction;
+      assert(hasFunction('matches').matches(hasMember()));
+      assert(!hasFunction('length').matches([]));
+      assert(!hasFunction('somethingElse').matches(null));
     }},
 
-    testInstanceOf: function() { with(this) {
-        var instanceOf = JsHamcrest.Matchers.instanceOf;
-        assert(instanceOf(Array).matches([]));
-        assert(instanceOf(Object).matches([]));
-        assert(!instanceOf(Function).matches([]));
-    }},
+  testInstanceOf: function() { with(this) {
+    var instanceOf = JsHamcrest.Matchers.instanceOf;
+    assert(instanceOf(Array).matches([]));
+    assert(instanceOf(Object).matches([]));
+    assert(!instanceOf(Function).matches([]));
+  }},
 
-    testTypeOf: function() { with(this) {
-        assert(typeOf('string').matches('text'));
-        assert(typeOf('number').matches(10));
-        assert(typeOf('boolean').matches(false));
-        assert(typeOf('function').matches(function() { }));
-    }},
+  testTypeOf: function() { with(this) {
+    assert(typeOf('string').matches('text'));
+    assert(typeOf('number').matches(10));
+    assert(typeOf('boolean').matches(false));
+    assert(typeOf('function').matches(function() { }));
+  }},
 
-    testObject: function() { with(this) {
-        assert(object().matches({}));
-        assert(!object().matches(10));
-    }},
+  testObject: function() { with(this) {
+    assert(object().matches({}));
+    assert(!object().matches(10));
+  }},
 
-    testString: function() { with(this) {
-        assert(string().matches('text'));
-        assert(!string().matches(10));
-    }},
+  testString: function() { with(this) {
+    assert(string().matches('text'));
+    assert(!string().matches(10));
+  }},
 
-    testNumber: function() { with(this) {
-        assert(number().matches(10));
-        assert(number().matches(10.0));
-        assert(!number().matches('text'));
-    }},
+  testNumber: function() { with(this) {
+    assert(number().matches(10));
+    assert(number().matches(10.0));
+    assert(!number().matches('text'));
+  }},
 
-    testBool: function() { with(this) {
-        assert(bool().matches(true));
-        assert(bool().matches(false));
-        assert(!bool().matches('text'));
-    }},
+  testBool: function() { with(this) {
+    assert(bool().matches(true));
+    assert(bool().matches(false));
+    assert(!bool().matches('text'));
+  }},
 
-    testFunc: function() { with(this) {
-        assert(func().matches(function() { }));
-        assert(!func().matches({}));
-    }}
+  testFunc: function() { with(this) {
+    assert(func().matches(function() { }));
+    assert(!func().matches({}));
+  }}
 }, {'testLog': 'objectLog'});
